b28f443a66d6eeac3162f401dd6ef309ffb83050
Raeef Refai <raeef.refai@gmail.com>
2017-07-22 23:51:41 +0300

    add backend service

---
+++ client/src/app/shared/backend.service.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+} from '@angular/core';
+import {
+  HttpClient,
+  HttpParams,
+} from '@angular/common/http';
+import {
+  Observable,
+} from 'rxjs/Observable';
+import {
+  environment,
+} from '../../environments/environment';
+
+@Injectable()
+export class BackendService {
+
+  constructor(
+    private _http: HttpClient,
+  ) { }
+
+  post(model: string, data: any, action?: string, params?: HttpParams): Observable<any> {
+    action = action ? `/${action}` : '';
+
+    const options = {
+      params: params || new HttpParams(),
+    };
+
+    return this._http.post(`${environment.baseApiUrl}/${model}${action}`, data, options);
+  }
+
+}
