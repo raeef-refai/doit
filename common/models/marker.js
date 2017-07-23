6f702c9020bde2f382b3cbbbb2ac93e36c6fa4a3
Raeef Refai <raeef.refai@gmail.com>
2017-07-23 10:02:47 +0300

    add account id to the marker when created

--- common/models/marker.js
+++ common/models/marker.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Marker) {
+  Marker.beforeRemote('create', (ctx, instance, next) => {
+    const data = ctx.args.data;
 
+    const accountId = ctx.req.accessToken.userId;
+
+    if (data.length) {
+      data.forEach((marker) => {
+        marker.accountId = accountId;
+      });
+    } else {
+      data.accountId = accountId;
+    }
+
+    next();
+  });
 };
